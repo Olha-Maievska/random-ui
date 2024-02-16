64243cc715ff085a8cbf3642a75926de20b13f6a
Olha Maievska <olhamaievska91@gmail.com>
2024-02-17 00:57:33 +0100

    add login flow tests

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,41 @@
+describe('Login flow', () => {
+  it('should redirect to login page', () => {
+    cy.visit('http://localhost:5173/')
+
+    cy.wait(1000)
+
+    cy.url().should('include', '/login')
+  })
+
+  it('should able to login', () => {
+    cy.session('login', () => {
+      cy.visit('http://localhost:5173/login')
+
+      cy.wait(1000)
+
+      cy.url().should('include', '/login')
+
+      cy.get('#email').type('hr@test.com')
+      cy.get('#password').type('test')
+
+      cy.get('form').submit()
+
+      cy.url().should('not.include', '/login')
+    })
+  })
+
+  it('should be adle to logout', () => {
+    cy.visit('http://localhost:5173/')
+
+    cy.wait(1000)
+
+    cy.get('#email').type('hr@test.com')
+    cy.get('#password').type('test')
+    cy.get('form').submit()
+
+    cy.wait(1000)
+
+    cy.get('button').contains('Log out').click()
+    cy.url().should('include', '/login')
+  })
+})
